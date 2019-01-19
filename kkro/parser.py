1f36de4f21a17e50c529bd82c33d89fe6da1af9c
Jahan Addison <jahan@jahanaddison.com>
2019-01-19 17:41:37 -0500

    grammar: finalizing

--- kkro/parser.py
+++ kkro/parser.py
@@ -4,25 +4,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 sample = '''
-main () {
-   auto j,s[20],t[20];
-   reread(); /* get command line */
-   getstr(s); /* put into s */
-   j = getarg(t,s,0); /* skip H* name */
-   j = getarg(t,s,j); /* filel */
-   openr( 5,t );
-   getarg(t,s,j); /* file2 */
-   openw( 6,t );
-   while( putchar( getchar() ) != '*e' ) ; /* copy contents */
-   }
 
 '''
 
 with open('kkro/grammar.lark') as file:
     grammar = file.read()
-    test = Lark(grammar,
-                start='program',
-                parser='earley',
-                lexer='dynamic',
-                debug=True)
-    print(test.parse(sample).pretty())
+    parser = Lark(grammar,
+                  start='program',
+                  parser='earley',
+                  lexer='dynamic',
+                  debug=True)
+    print(parser.parse(sample).pretty())
