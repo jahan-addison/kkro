cb8f6cb36730dec91cdc630299401ca12d839fa6
Jahan Addison <jahan@jahanaddison.com>
2024-12-11 09:00:20 -0500

    feat: factory functions and bug fixes

--- test/test_xion.py
+++ test/test_xion.py
@@ -3,13 +3,14 @@ from os import getcwd
 
 from xion import __version__
 from xion.parser import Parser
+from xion.parser import parse_source_program, parse_source_program_as_string
 
 from .fixture.program_1_parse_tree import program_example_1_parse_tree
 from .fixture.program_2_parse_tree import program_example_2_parse_tree
 from .fixture.program_3_parse_tree import program_example_3_parse_tree
 
 def test_version() -> None:
-    assert __version__ == '1.0.1'
+    assert __version__ == '1.0.2'
 
 def test_program_1_parse_tree(program_example_1_parse_tree: str) -> None:
     with open(getcwd() + '/examples/1.b') as file:
@@ -26,4 +27,14 @@ def test_program_3_parse_tree(program_example_3_parse_tree: str) -> None:
         parser = Parser(file.read())
 
         assert(str(parser).replace('\t', ' ' * 4) == program_example_3_parse_tree)
+
+
+def test_parse_source_program(program_example_1_parse_tree: str) -> None:
+    with open(getcwd() + '/examples/1.b') as file:
+        assert(str(parse_source_program(file.read())) == program_example_1_parse_tree)
+
+def test_parse_source_program_as_string(program_example_3_parse_tree: str) -> None:
+    with open(getcwd() + '/examples/3.b') as file:
+        parse_tree = parse_source_program_as_string(file.read())
+        assert(parse_tree.replace('\t', ' ' * 4) == program_example_3_parse_tree)
 
