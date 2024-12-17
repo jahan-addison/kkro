6c66b336200d3ed471998d5c363fb2be1f0373d8
Jahan Addison <jahan@jahanaddison.com>
2024-12-16 21:46:20 -0500

    feat(ast) ast transformer improvements and tests

--- test/test_xion.py
+++ test/test_xion.py
@@ -4,13 +4,14 @@ from os import getcwd
 from xion import __version__
 from xion.parser import Parser
 from xion.parser import parse_source_program, parse_source_program_as_string
-
+from xion.parser import get_source_program_as_ast, get_source_program_ast_as_string
 from .fixture.program_1_parse_tree import program_example_1_parse_tree
 from .fixture.program_2_parse_tree import program_example_2_parse_tree
 from .fixture.program_3_parse_tree import program_example_3_parse_tree
+from .fixture.program_1_ast import program_example_1_ast
 
 def test_version() -> None:
-    assert __version__ == '1.0.2'
+    assert __version__ == '1.0.3'
 
 def test_program_1_parse_tree(program_example_1_parse_tree: str) -> None:
     with open(getcwd() + '/examples/1.b') as file:
@@ -28,7 +29,6 @@ def test_program_3_parse_tree(program_example_3_parse_tree: str) -> None:
 
         assert(str(parser).replace('\t', ' ' * 4) == program_example_3_parse_tree)
 
-
 def test_parse_source_program(program_example_1_parse_tree: str) -> None:
     with open(getcwd() + '/examples/1.b') as file:
         assert(str(parse_source_program(file.read())) == program_example_1_parse_tree)
@@ -37,4 +37,12 @@ def test_parse_source_program_as_string(program_example_3_parse_tree: str) -> No
     with open(getcwd() + '/examples/3.b') as file:
         parse_tree = parse_source_program_as_string(file.read())
         assert(parse_tree.replace('\t', ' ' * 4) == program_example_3_parse_tree)
+
+def test_get_source_program_as_ast(program_example_1_ast: str) -> None:
+    with open(getcwd() + '/examples/1.b') as file:
+        assert(str(get_source_program_as_ast(file.read())) == program_example_1_ast)
+
+def test_get_source_program_ast_as_string(program_example_1_ast: str) -> None:
+    with open(getcwd() + '/examples/1.b') as file:
+        assert(get_source_program_ast_as_string(file.read(), pretty=False) == program_example_1_ast)
 
--- xion/transformer.py
+++ xion/transformer.py
@@ -1,22 +1,22 @@
-from lark import Transformer, Discard
+from lark import Tree, Transformer, Discard
 
 
 class AST_Transformer(Transformer):
     def constant_expression(self, args):
-        """Reduce constant expressions into their value """
+        """Reduce constant expression trees to their children"""
         return args[0]
 
     def number_literal(self, args):
-        """Merge number literals and turn them into ints """
-        return int(''.join(args))
+        """Merge number literals and turn them into an int """
+        return Tree('number_literal', [int(''.join(args))])
 
     def string_literal(self, args):
         """Merge string literals """
-        return ''.join(args)
+        return Tree('string_literal', [''.join(args)])
 
     def constant_literal(self, args):
         """Merge constant literals """
-        return ''.join(args)
+        return Tree('constant_literal', [''.join(args)])
 
     def TERMINATE(self, name):
         """Throw away ';' """
